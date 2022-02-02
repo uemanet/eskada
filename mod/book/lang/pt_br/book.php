f5186891217bfc1bb8d8549ce6bb4a57d44881e5
Willian Mano <willianmanoaraujo@gmail.com>
2022-02-02 12:23:54 -0300

    Adiciona pt br

---
+++ mod/book/lang/pt_br/book.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Book module language strings
+ *
+ * @package    mod_book
+ * @copyright  2004-2012 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['modulename'] = 'Livro';
+
+$string['bookviewreport'] = 'Relatório de visualização do livro';
+$string['book:viewreports'] = 'Ver relatórios do livro';
+$string['reportindextitle'] = 'Relatórios do livro';
+$string['totalviews'] = 'Total de visualizações';
+$string['uniqueuserviews'] = 'Visualizações de usuários únicos';
+$string['bookreadreport'] = 'Progresso de leitura dos estudantes';
+$string['reportreadtitle'] = 'Progresso de leitura dos estudantes';
+$string['readprogress'] = 'Progresso de leitura';
+
+$string['readpercent_ruledesc'] = 'Ler {$a}% do livro';
+$string['requiredreadpercent'] = 'O usuário precisa ler pelo menos este percentual do livro:';
+$string['readpercentselect'] = 'Requer percentual de leitura';
+$string['readpercentvalidation'] = 'Você precisa definir o percentual de leitura obrigatório';
+
